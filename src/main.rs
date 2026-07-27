5db415bd25817bf05ac3d48fc2360f461a124039
Vandanjon <vandanjon@gmail.com>
2026-07-27 15:02:45 +0200

    [FEAT] : display an empty window on start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::egui;
+
+#[derive(Default)]
+struct GameApp;
+
+
+impl eframe::App for GameApp {
+    fn ui(
+        &mut self,
+        ui: &mut egui::Ui,
+        _frame: &mut eframe::Frame,
+    ) {
+        ui.label("Bonjour");
+    }
+}
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+
+    eframe::run_native(
+        "L'Oasis",
+        options,
+        Box::new(|_cc| Ok(Box::new(GameApp))),
+    )
 }
