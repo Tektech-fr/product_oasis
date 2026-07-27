659f410fdc696d8e96206808edd61bcfc7166707
Vandanjon <vandanjon@gmail.com>
2026-07-27 21:56:25 +0200

    [FEAT] : add background image + display fullscreen

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,35 @@ struct GameApp;
 
 impl eframe::App for GameApp {
     fn ui(
-        &mut self,
-        ui: &mut egui::Ui,
-        _frame: &mut eframe::Frame,
+        &mut self, 
+        ui: &mut egui::Ui, 
+        _frame: &mut eframe::Frame
     ) {
+        egui::CentralPanel::default()
+            .frame(egui::Frame::NONE)
+            .show(ui, |ui| {
+            egui::Image::new(egui::include_image!("../assets/bg_main_green_grass.webp"))
+            .fit_to_exact_size(ui.max_rect().size())
+            .paint_at(ui, ui.max_rect());
+
         ui.label("Bonjour");
-    }
+        });
+        }
 }
 
 fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+        .with_fullscreen(true),
+        ..Default::default()
+    };
 
     eframe::run_native(
         "L'Oasis",
         options,
-        Box::new(|_cc| Ok(Box::new(GameApp))),
-    )
+        Box::new(|cc| {
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+         Ok(Box::new(GameApp))
+    }),
+)
 }
