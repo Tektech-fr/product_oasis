97947628a4429215d10556d9188f1a75b0a58e68
Vandanjon <vandanjon@gmail.com>
2026-07-28 18:22:58 +0200

    [REFACTO] : move background display responsability to right files

--- src/game/game_app.rs
+++ src/game/game_app.rs
@@ -21,25 +21,9 @@ impl eframe::App for GameApp {
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default()
             .frame(egui::Frame::NONE)
-            .show(ui, |ui| {
-                let background = match self.state {
-                    AppState::MainMenu => egui::include_image!("../../assets/bg_branded.webp"),
-                    AppState::Playing => {
-                        egui::include_image!("../../assets/bg_green_grass.webp")
-                    }
-                };
-
-                egui::Image::new(background)
-                    .fit_to_exact_size(ui.max_rect().size())
-                    .paint_at(ui, ui.max_rect());
-
-                if self.state == AppState::MainMenu {
-                    self.show_main_menu(ui);
-                }
-
-                if self.state == AppState::Playing {
-                    self.play_game(ui);
-                }
+            .show(ui, |ui| match self.state {
+                AppState::MainMenu => self.show_main_menu(ui),
+                AppState::Playing => self.play_game(ui),
             });
     }
 }
--- src/game/playing.rs
+++ src/game/playing.rs
@@ -4,6 +4,13 @@ use super::game_app::{AppState, GameApp};
 
 impl GameApp {
     pub(super) fn play_game(&mut self, ui: &mut egui::Ui) {
+        let screen = ui.max_rect();
+        let background = egui::include_image!("../../assets/bg_green_grass.webp");
+
+        egui::Image::new(background)
+            .fit_to_exact_size(screen.size())
+            .paint_at(ui, ui.max_rect());
+
         if ui.input(|i| i.key_pressed(egui::Key::Escape)) {
             self.state = AppState::MainMenu;
 
