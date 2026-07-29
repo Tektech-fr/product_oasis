696ce5b3afd7f7f184baf564c76e71c1feff5f14
Vandanjon <vandanjon@gmail.com>
2026-07-29 21:54:09 +0200

    [FEAT] : draw zones on the board game

--- src/game/zones.rs
+++ src/game/zones.rs
@@ -2,8 +2,8 @@ use eframe::egui;
 
 pub(super) struct Zone {
     title: String,
-    left: f32,
-    top: f32,
+    center_x: f32,
+    center_y: f32,
     width: f32,
     height: f32,
 }
@@ -11,25 +11,25 @@ pub(super) struct Zone {
 impl Zone {
     pub(super) fn new(
         title: impl Into<String>,
-        left: usize,
-        top: usize,
+        center_x: usize,
+        center_y: usize,
         width: usize,
         height: usize,
     ) -> Self {
         Self {
             title: title.into(),
-            left: left as f32 * 0.01,
-            top: top as f32 * 0.01,
+            center_x: center_x as f32 * 0.01,
+            center_y: center_y as f32 * 0.01,
             width: width as f32 * 0.01,
             height: height as f32 * 0.01,
         }
     }
 
     fn rect_within(&self, board_rect: egui::Rect) -> egui::Rect {
-        let min = board_rect.min
+        let center = board_rect.min
             + egui::vec2(
-                self.left * board_rect.width(),
-                self.top * board_rect.height(),
+                self.center_x * board_rect.width(),
+                self.center_y * board_rect.height(),
             );
 
         let size = egui::vec2(
@@ -37,20 +37,22 @@ impl Zone {
             self.height * board_rect.height(),
         );
 
-        egui::Rect::from_min_size(min, size)
+        egui::Rect::from_center_size(center, size)
     }
 
     pub(super) fn draw(&self, ui: &mut egui::Ui, board_rect: egui::Rect) {
         let rect = self.rect_within(board_rect);
 
         ui.painter()
-            .rect_filled(rect, 0, egui::Color32::from_black_alpha(60));
+            .rect_filled(rect, 0, egui::Color32::from_black_alpha(80));
+
         ui.painter().rect_stroke(
             rect,
             0,
             egui::Stroke::new(1.5, egui::Color32::WHITE),
             egui::StrokeKind::Inside,
         );
+
         ui.painter().text(
             rect.center(),
             egui::Align2::CENTER_CENTER,
